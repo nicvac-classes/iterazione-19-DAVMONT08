aafeb2a48f97e97a56219d742ae6849e22e0044b
DAVMONT08 <davide.montella@marconibari.edu.it>
2024-11-07 09:16:57 +0000

    CONSEGNA

--- main.cpp
+++ main.cpp
@@ -1,11 +1,33 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+int main() { 
+               int n,d,nd;
+               cout<<"inserisci un numero intero"<<endl;
+               cin>>n;
+               d=1;
+               nd=0;
+               {
+                   while(d<=n)
+                   if(n%d=0)
+                   {
+                    cout<< d << "è divisore"<<endl;
+                    nd=nd+1;
+                   }
+                   d=d+1;
+
+               }
+                if(nd=2)
+                {
+                    cout<< n << "è un numero primo"<<endl;
+                }
+            else
+            {
+                cout<< n << " non è un numero primo"<<endl;
+            }
+        cout<< n << "ha" << nd << "divisori";
+
+   
 }
 
 //LEGGERE LE ISTRUZIONI NEL FILE README.md
